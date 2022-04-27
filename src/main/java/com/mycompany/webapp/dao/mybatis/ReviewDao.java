565f96af9737d9a9a1c39052896909b20f7384af
lee0507 <2eastbow@gmail.com>
2022-04-27 13:47:14 +0900

    RewviewDao, RewviewService, Rewview xml에 userid별 count 추가, review delete메소드 삭제

--- src/main/java/com/mycompany/webapp/dao/mybatis/ReviewDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/ReviewDao.java
@@ -13,7 +13,7 @@ public interface ReviewDao {
 	public List<Review> selectByPage(Pager pager);
 	public List<Review> selectByUserid(@Param("userid") String userid, @Param("pager") Pager pager);
 	public int count();
+	public int countByUserid(String userid);
 	public int insert(Review reviews);
-	public int update(Review reviews); 		//수정이랑
-	public int deleteByReviewno(int reviewno); 	//삭제도 일단 혹시 몰라서 만들어 놓았음.
+	public int update(Review reviews);
 }
--- src/main/java/com/mycompany/webapp/service/ReviewService.java
+++ src/main/java/com/mycompany/webapp/service/ReviewService.java
@@ -19,28 +19,33 @@ public class ReviewService {
 	@Resource
 	private ReviewDao reviewDao;
 
+	//페이지네이션을 위한 전체 리뷰 갯수
 	public int getTotalReviewCount() {
 		return reviewDao.count();
 	}
+	
+	//페이지네이션을 위한 user의 전체 리뷰 갯수
+	public int getTotalReviewCountByUserid() {
+		return reviewDao.count();
+	}
 
+	//전체 리뷰 조회
 	public List<Review> getReviews(Pager pager) {
 		return reviewDao.selectByPage(pager);
 	}
-
+	
+	//user의 전체 리뷰 조회
 	public List<Review> getReviewsByUserid(String userid, Pager pager) {
 		return reviewDao.selectByUserid(userid, pager);
 	}
-
+	
+	//리뷰 작성
 	public void writeReview(Review reviews) {
 		reviewDao.insert(reviews);
 	}
-
+	
+	//리뷰 수정
 	public void updateReview(Review reviews) {
 		reviewDao.update(reviews);
 	}
-
-	public void removeByReviewno(int reviewno) {
-		reviewDao.deleteByReviewno(reviewno);
-	}
-
 }
