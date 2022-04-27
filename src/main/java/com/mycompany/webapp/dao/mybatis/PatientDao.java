20e45147fbc7fa587f15957c1c5fb85d4be9b2bf
lee0507 <2eastbow@gmail.com>
2022-04-27 11:01:53 +0900

    patientDao 및 patient.xml에 이름으로 검색 추가

--- src/main/java/com/mycompany/webapp/dao/mybatis/PatientDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/PatientDao.java
@@ -8,8 +8,9 @@ import com.mycompany.webapp.dto.Patient;
 
 @Mapper
 public interface PatientDao {
-	public Patient selectByPatientssn(String patientssn);
-	public List<Patient> selectAll();
+	public Patient selectByPatientssn(String patientssn);	//주민번호로 환자 검색
+	public List<Patient> selectByPatientName(String patientname);	//이름으로 환자 검색
+	public List<Patient> selectAll();	//전체 환자 출력용
 	public int insert(Patient patient);
 	public int update(Patient patient);
 	public int deleteByPatientssn(String patientssn);
