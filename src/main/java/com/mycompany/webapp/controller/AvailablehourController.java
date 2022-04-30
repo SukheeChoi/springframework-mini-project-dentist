bf25444bd9b46e128e6349f543753d2b32a1fd7d
lee0507 <2eastbow@gmail.com>
2022-04-30 19:21:20 +0900

    달력에 데이터 뿌리기 위한 병원쪽 컨트롤러 추가 및 매퍼 수정

---
+++ src/main/java/com/mycompany/webapp/controller/AvailablehourController.java
@@ -0,0 +1,41 @@
+package com.mycompany.webapp.controller;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mycompany.webapp.dto.Availablehour;
+import com.mycompany.webapp.service.AvailablehourService;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/availablehour")
+@Log4j2
+public class AvailablehourController {
+	
+	@Resource
+	private AvailablehourService availablehourService;
+	
+	@RequestMapping(value="/getHour", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public String getHour(Date date, HttpServletResponse response) {
+		/*
+		 요청은 이런식 => getHour?date=2022/05/06
+		 http://localhost:8080/springframework-mini-project-dentist/availablehour/getHour?date=2022/05/06
+		 응답은 이런식 => {"date", "0000000001111111111100000000000"}
+		*/
+		Availablehour dbdate = availablehourService.selectBydate(date);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("date", dbdate.getAvailabletime());
+		String json = jsonObject.toString();
+		log.info(dbdate);
+		return json;
+	}
+}
--- src/main/java/com/mycompany/webapp/dto/Availablehour.java
+++ src/main/java/com/mycompany/webapp/dto/Availablehour.java
@@ -6,6 +6,6 @@ import lombok.Data;
 
 @Data
 public class Availablehour {
-	private Date Availabledate;
-	private String Availabletime;
+	private Date availabledate;
+	private String availabletime;
 }
