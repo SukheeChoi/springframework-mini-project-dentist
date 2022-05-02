03078971b5e9d5989e41cf308e222116c0530273
zzeongm <ljm5937@gmail.com>
2022-05-02 21:51:11 +0900

    reservation business service dao controll 수정

--- src/main/java/com/mycompany/webapp/controller/BusinesshourController.java
+++ src/main/java/com/mycompany/webapp/controller/BusinesshourController.java
@@ -11,29 +11,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mycompany.webapp.dto.Availablehour;
+import com.mycompany.webapp.dto.Businesshour;
 import com.mycompany.webapp.service.AvailablehourService;
+import com.mycompany.webapp.service.BusinesshourService;
 
 import lombok.extern.log4j.Log4j2;
 
 @Controller
-@RequestMapping("/availablehour")
+@RequestMapping("/businesshour")
 @Log4j2
-public class AvailablehourController {
+public class BusinesshourController {
 	
 	@Resource
-	private AvailablehourService availablehourService;
+	private BusinesshourService businesshourService;
 	
 	@GetMapping(value="/getHour", produces = "application/json; charset=UTF-8")
 	@ResponseBody
-	public String getHour(Date date) {
+	public String getHour(String businessday) {
 		/*
 		 요청은 이런식 => getHour?date=2022/05/06
-		 http://localhost:8080/springframework-mini-project-dentist/availablehour/getHour?date=2022/05/06
+		 http://localhost:8080/springframework-mini-project-dentist/businesshour/getHour?businessday=2022/05/06
 		 응답은 이런식 => {"date", "0000000001111111111100000000000"}
 		*/
-		Availablehour dbdate = availablehourService.selectBydate(date);
+		Businesshour dbdate = businesshourService.selectByBusinessday(businessday);
 		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("date", dbdate.getAvailabletime());
+		jsonObject.put("businessday", dbdate.getBusinesshour());
 		String json = jsonObject.toString();
 		return json;
 	}
