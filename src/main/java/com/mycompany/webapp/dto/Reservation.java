ef3db16077771d257371b493ac566145051f8e14
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-05-04 21:38:07 +0900

    ..

--- src/main/java/com/mycompany/webapp/dto/Reservation.java
+++ src/main/java/com/mycompany/webapp/dto/Reservation.java
@@ -1,6 +1,8 @@
 package com.mycompany.webapp.dto;
 
-import java.util.Date;
+import javax.annotation.Resource;
+
+import com.mycompany.webapp.service.DeninfoService;
 
 import lombok.Data;
 
@@ -23,10 +25,13 @@ public class Reservation {
 	private String dendomain;
 	private String denname;
 	
+	@Resource
+	DeninfoService deninfoService;
+	
 	public Reservation() {
 		//DB에서 동적으로 가져와야 함.(IP주소는 고유하지만 변경 가능하기 때문.)
-		this.dendomain = "8082";
-		this.denname = "광야치과";
+		this.dendomain = deninfoService.selectDeninfo().getDendomain();
+		this.denname = deninfoService.selectDeninfo().getDenname();
 	}
 	
 }
